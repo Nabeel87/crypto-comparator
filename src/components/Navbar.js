d37055638ce14eb0387fb3e44c52000f152bb798
Nabeel87 <nabilsabir786@gmail.com>
2025-07-18 13:48:59 +0500

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+    <ul className="navbar">
+        <li>
+            <NavLink to="/" className={({isActive}) => isActive ? 'active' : ''} >
+                Home
+            </NavLink> 
+            <NavLink to="/compare" className={({isActive}) => isActive ? 'active' : ''} >
+                Compare
+            </NavLink> 
+            <NavLink to="/favorites" className={({isActive}) => isActive ? 'active' : ''} >
+                Favorites
+            </NavLink> 
+        </li>
+    </ul>
+  )
+}
+
+export default Navbar
