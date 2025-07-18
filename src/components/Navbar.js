4185b50bb2a12564f3f3fdecec2f96d791d52953
Nabeel87 <nabilsabir786@gmail.com>
2025-07-18 18:57:00 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,20 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-    <ul className="navbar">
-        <li>
-            <NavLink to="/" className={({isActive}) => isActive ? 'active' : ''} >
-                Home
-            </NavLink> 
-            <NavLink to="/compare" className={({isActive}) => isActive ? 'active' : ''} >
-                Compare
-            </NavLink> 
-            <NavLink to="/favorites" className={({isActive}) => isActive ? 'active' : ''} >
-                Favorites
-            </NavLink> 
-        </li>
-    </ul>
-  )
+        <ul className="navbar">
+            <li>
+                <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''} >
+                    Home
+                </NavLink>
+                <NavLink to="/compare" className={({ isActive }) => isActive ? 'active' : ''} >
+                    Compare
+                </NavLink>
+                <NavLink to="/favorites" className={({ isActive }) => isActive ? 'active' : ''} >
+                    Favorites
+                </NavLink>
+            </li>
+        </ul>
+    )
 }
 
 export default Navbar
