c65b7b501732a090f6ba8c12d1b2e8f5b4683a4b
Nabeel87 <nabilsabir786@gmail.com>
2025-07-18 14:08:06 +0500

    Routing Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Compare from './pages/Compare';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/compare' element={<Compare />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
     </div>
   );
 }
