cdebd8ac67e1ce34b9c0cbabdd8f381249913220
Nabeel87 <nabilsabir786@gmail.com>
2025-07-19 22:46:11 +0500

    Update index.js
    wrap app in crypto provider
    sequence is very important for avoding error
     <React.StrictMode>
        <ToggleThemeProvider>
         <CryptoProvider>
            <BrowserRouter>
              <App />
            </BrowserRouter>
          </CryptoProvider>
        </ToggleThemeProvider>
      </React.StrictMode>

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import {ToggleThemeProvider} from './ToggleThemeContext';
+import { ToggleThemeProvider } from './ToggleThemeContext';
+import { CryptoProvider } from './context/CryptoContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ToggleThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <CryptoProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CryptoProvider>
     </ToggleThemeProvider>
   </React.StrictMode>
 );
