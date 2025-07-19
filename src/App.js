83bff740d3d303ac30fba085bc3c38a3489b75ee
Nabeel87 <nabilsabir786@gmail.com>
2025-07-19 22:11:01 +0500

    Update App.js
    side bar, footer, header added and use <box> from MUi

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Box, Toolbar } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
-import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Sidebar from './components/SideBar';
+import Footer from './components/Footer';
+
 import Compare from './pages/Compare';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
+import Trends from './pages/Trends';
 
 function App() {
+
+  const theme = useTheme();
+
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/compare' element={<Compare />} />
-        <Route path='/favorites' element={<Favorites />} />
-      </Routes>
-    </div>
+  
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Header />
+      <Box sx={{ display: 'flex', flexGrow: 1, bgcolor: theme.palette.background.default }}>
+        <Sidebar />
+        <Box sx={{ flexGrow: 1, p: 3 }}>
+          {/* For spacing under AppBar */}
+          <Toolbar /> 
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/compare' element={<Compare />} />
+            <Route path='/favorites' element={<Favorites />} />
+            <Route path='/trends' element={<Trends />} />
+          </Routes>
+        </Box>
+      </Box>
+      <Footer />
+    </Box>
   );
 }
 
