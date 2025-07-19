cda01e719a30a85b2a253a8b684229591d61e7eb
Nabeel87 <nabilsabir786@gmail.com>
2025-07-19 22:51:39 +0500

    Update Home.js
    show top 100 coins on page using ul li

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,23 @@ import React from 'react'
 import CryptoContext from '../context/CryptoContext';
 
 const Home = () => {
+
+  const { coins, loading, error } = useContext(CryptoContext);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
-    <div>Home</div>
+    <div>
+      <h2>Top 100 Cryptos</h2>
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} - ${coin.current_price}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 };
 
