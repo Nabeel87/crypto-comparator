6088810dc6110ac70b082817beca3d2764ce893d
Nabeel87 <nabilsabir786@gmail.com>
2025-07-19 22:14:11 +0500

    Update index.js
    wrap <App /> with  <ToggleThemeProvider>
          <BrowserRouter>
    for routing and theme Change

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {ToggleThemeProvider} from './ToggleThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ToggleThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ToggleThemeProvider>
   </React.StrictMode>
 );
 
